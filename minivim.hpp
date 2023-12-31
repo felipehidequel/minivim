9f3660f764c8561fe12dc795a18101d1484170bb
felipehidequel <felipehidequel@gmail.com>
2023-12-31 11:51:14 -0300

    first commit

---
+++ minivim.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <ncurses.h>
+#include <memory>
+
+class minivim
+{
+private:
+    /* data */
+public:
+    minivim(/* args */);
+    ~minivim();
+};
+
+minivim::minivim(/* args */)
+{
+}
+
+minivim::~minivim()
+{
+}
